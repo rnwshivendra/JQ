6c9b49388fb8e6e45c997fed8576f6f6742301aa
rnwshivendra <rwnv1.shivendra.ds@gmail.com>
2024-04-22 09:58:29 +0530

    task

---
+++ L1/script.js
@@ -0,0 +1,10 @@
+
+//   $("id or class or tag or combinators")  ----> jquery selector
+// .on("event" , function)  --->  1. event   2. function
+$("#text").hide();
+
+$("button").on("click",function(){
+    $("#text").show();
+})
+
+
